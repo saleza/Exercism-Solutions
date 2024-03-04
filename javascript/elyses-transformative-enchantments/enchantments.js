4e43c6717541e92c5ef81421fadadcb7413ac9f5
neidrasa <adrien.salez@outlook.fr>
2024-03-04 17:20:11 +0100

    Save 04/03

--- javascript/elyses-transformative-enchantments/enchantments.js
+++ javascript/elyses-transformative-enchantments/enchantments.js
@@ -22,9 +22,9 @@ export function threeOfEachThree(deck) {
   for(let i = 0; i < deck.length; i++) {
     if (deck[i] === 3) {
       deck.splice(i, 0, 3, 3)
+      i = i + 3
     }
   }
-
   return deck;
 }
 
@@ -37,7 +37,7 @@ export function threeOfEachThree(deck) {
  * @returns {number[]} deck with only two middle cards
  */
 export function middleTwo(deck) {
-  throw new Error('Implement the middleTwo function');
+  return deck.slice()
 }
 
 /**
