83665533df9f2f8cad91ebd14f8ca3f5d0b97a40
neidrasa <adrien.salez@outlook.fr>
2024-10-11 01:47:40 +0200

    Constructor exercice

--- java/need-for-speed/src/main/java/NeedForSpeed.java
+++ java/need-for-speed/src/main/java/NeedForSpeed.java
@@ -1,31 +1,45 @@
 class NeedForSpeed {
+    private int speed;
+    private int battery = 100;
+    private int batteryDrain;
+    private int distance;
+
     NeedForSpeed(int speed, int batteryDrain) {
-        throw new UnsupportedOperationException("Please implement the NeedForSpeed constructor");
+        this.speed = speed;
+        this.batteryDrain = batteryDrain;
     }
 
     public boolean batteryDrained() {
-        throw new UnsupportedOperationException("Please implement the NeedForSpeed.batteryDrained() method");
+        return this.batteryDrain > this.battery;
     }
 
     public int distanceDriven() {
-        throw new UnsupportedOperationException("Please implement the NeedForSpeed.distanceDriven() method");
+        return this.distance;
     }
 
     public void drive() {
-        throw new UnsupportedOperationException("Please implement the NeedForSpeed.drive() method");
+        if (this.battery > 0) {
+        this.distance += this.speed;
+        this.battery -= this.batteryDrain;
+        }
     }
 
     public static NeedForSpeed nitro() {
-        throw new UnsupportedOperationException("Please implement the (static) NeedForSpeed.nitro() method");
+        return new NeedForSpeed(50,4);
     }
 }
 
 class RaceTrack {
+    private int distance;
+
     RaceTrack(int distance) {
-        throw new UnsupportedOperationException("Please implement the RaceTrack constructor");
+        this.distance = distance;
     }
 
     public boolean canFinishRace(NeedForSpeed car) {
-        throw new UnsupportedOperationException("Please implement the RaceTrack.canFinishRace() method");
+        while (car.distanceDriven() < this.distance && !car.batteryDrained()) {
+            car.drive();
+        }
+        return car.distanceDriven() >= this.distance;
     }
 }
